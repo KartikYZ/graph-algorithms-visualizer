8268bfb81504706d1e34ddd6156af649db937c58
KartikYZ <67892980+KartikYZ@users.noreply.github.com>
2021-06-23 15:11:38 +0530

    placeholder "set" class

---
+++ src/utils/hashSet.ts
@@ -0,0 +1,50 @@
+export default class HashSet<T> {  // implemented as a list for now.
+
+    private set: T[];
+    private size: number;
+
+    constructor() {
+        this.set = new Array<T>();
+        this.size = 0;
+    }
+
+    add(obj: T): void {
+        this.set.push(obj);
+        this.size++;
+    }
+
+    remove(obj: any): void {
+        let index = -1;
+        for (let i = 0; i < this.set.length; i++) {
+            if (obj.equals(this.set[i])) {
+                index = i;
+                break;
+            }
+        }
+        this.set.splice(index, 1);
+    }
+
+    contains(obj: any) {
+        for (let i = 0; i < this.set.length; i++) {
+            if (obj.equals(this.set[i])) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    getSize(): number {
+        return this.size
+    }
+
+    clear(): void {
+        this.set = [];
+        this.size = 0;
+    }
+
+    toString(): string {
+        return '';
+    }
+}
+
+// property equals does not exist on type T. Change any to T.
